600d3dc464d7d3c6969943ab07d4d11658693443
evelyneee <delorean3344@gmail.com>
2022-02-27 20:02:50 -0800

    cors

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,12 +8,18 @@ func routes(_ app: Application) throws {
     }
 
     app.get(version, "playing") { req -> Response in
+        print(req.url)
         let headers = HTTPHeaders([
             ("Authorization", Environment.get("DEV_TOKEN")!),
             ("Media-User-Token", Environment.get("USER_TOKEN")!),
             ("Content-Type","application/json")
         ])
         let request = try await req.client.get(URI.init(string: "https://api.music.apple.com/v1/me/recent/played/tracks"), headers: headers)
+        return Response(status: .ok, version: .http2, headers: [:], body: Response.Body(buffer: request.body ?? .init()))
+    }
+    
+    app.get("v1-alpha.1", "links") { req -> Response in
+        let request = try await req.client.get(URI.init(string: "https://api.song.link" + req.url.string), headers: [:])
         return Response(status: .ok, version: .http2, headers: [:], body: Response.Body(buffer: request.body ?? .init()))
     }
 }
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -6,4 +6,15 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 defer { app.shutdown() }
 try configure(app)
+let corsConfiguration = CORSMiddleware.Configuration(
+    allowedOrigin: .all,
+    allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+    allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin, HTTPHeaders.Name.init("content"), HTTPHeaders.Name.init("identifier")]
+)
+
+let cors = CORSMiddleware(configuration: corsConfiguration)
+// cors middleware should come before default error middleware using `at: .beginning`
+app.middleware.use(cors, at: .beginning)
+app.http.server.configuration.hostname = "127.0.0.1"
+app.http.server.configuration.port = 8080
 try app.run()
